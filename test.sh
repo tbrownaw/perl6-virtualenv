ff7215c0a262cf8b66d5ab5352595348641ec66a
Timothy Brownawell <tbrownaw@prjek.net>
2018-10-12 14:13:29 -0400

    Add a (failing) test.

---
+++ test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+env >initial.env
+
+test ! -e env 
+./pl6-virt env
+test -d env
+test -f env/bin/activate
+
+env >setup.env
+cmp initial.env setup.env
+
+. env/bin/activate
+
+env >active.env
+
+deactivate
+
+env >deactivated.env
+cmp setup.env deactivated.env
+
+. env/bin/activate
+NUM_BEFORE=$(find env | wc -l)
+! zef list --installed | grep -q YAMLish
+zef install YAMLish
+NUM_AFTER=$(find env | wc -l)
+
+test $NUM_AFTER -gt $NUM_BEFORE
+
+zef list --installed | grep -q YAMLish
+
+deactivate
+! zef list --installed | grep -q YAMLish
+
+rm -f active.env deactivated.env setup.env
